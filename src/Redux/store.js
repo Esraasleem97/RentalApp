0a6fdd8e99f325c08270d0a39e1dadedb5fdeb7b
programming 1 <Prog-1@yarub.com>
2021-06-19 21:48:52 +0300

    redux

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,6 @@
 import thunk from 'redux-thunk';
 import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {loginReducer} from './../Reducer/index';
+
 
 const rootReducer = combineReducers({
 
